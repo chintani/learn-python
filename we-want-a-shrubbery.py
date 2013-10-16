686c24f3aba2e4ebef4bba06f6a585fd05eee00c
Tania Chin <cybercorpindustries@gmail.com>
2013-10-15 22:27:49 -0400

    1.0.0
    First program.

---
+++ we-want-a-shrubbery.py
@@ -0,0 +1,9 @@
+# program template for mini-project 0
+
+# Modify the print statement according to 
+# the mini-project instructions
+
+print "We want... a shrubbery!"
+
+# Instead of the usual "Hello World" my first offical class
+# learning of Python was to print the statement above.
