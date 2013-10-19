c5f1deb0eb1e4afdf51af4328ab37c29cdef82b1
Tania Chin <cybercorpindustries@gmail.com>
2013-10-19 11:55:19 -0400

    1.0.8
    Filled in the last piece of the code for function rpsls, 1st draft.

--- rpsls-game.py
+++ rpsls-game.py
@@ -5,7 +5,7 @@ Created on Oct 16, 2013
 '''
 # Rock-paper-scissors-lizard-Spock template
 
-
+import random
 # The key idea of this program is to equate the strings
 # "rock", "paper", "scissors", "lizard", "Spock" to numbers
 # as follows:
@@ -61,19 +61,26 @@ def name_to_number(name):
 
 #def rpsls(name): 
     # fill in your code below
-
-    # convert name to player_number using name_to_number
-
-    # compute random guess for comp_number using random.randrange()
-
-    # compute difference of player_number and comp_number modulo five
-
-    # use if/elif/else to determine winner
-
-    # convert comp_number to name using number_to_name
     
-    # print results
-
+name = name_to_number # name = name_to_number
+name_to_number = player_number # convert name to player_number using name_to_number
+comp_number = random.range(5)# compute random guess for comp_number using random.randrange()
+  
+difference = (player_number - comp_number)% 5# compute difference of player_number and comp_number modulo five
+# use if/elif/else to determine winner
+    if difference == 1 or 2:
+        winner = Player 
+    elif difference == 3 or 4:
+        winner = Computer 
+    else:
+        winner = Player and Computer tie
+    
+ comp_number == number_to_name # convert comp_number to name using number_to_name
+    
+# print results
+print "Player chooses" player_number
+print "Computer chooses" comp_number
+print 
     
 # test your code
 #rpsls("rock")
