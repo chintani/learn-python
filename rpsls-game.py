e9a451fc8273255cf022ff37222d34cc77437959
Tania Chin <cybercorpindustries@gmail.com>
2013-10-20 22:49:34 -0400

    1.0.9
    Undo the changes I in commits 1.0.6 & 1.0.7 because the code was getting
    to be impossible to understand.

--- rpsls-game.py
+++ rpsls-game.py
@@ -20,67 +20,68 @@ import random
 
 def number_to_name(number):
     # fill in your code below
-    if number == 0:
-        name_to_number = "rock"
+    
+    if number == 0: # convert number to a name using if/elif/else
+        name = "rock"
     elif number == 1:
-        name_to_number = 'Spock'
+        name = "Spock"
     elif number == 2:
-        name_to_number = 'paper'
+        name = "paper"
     elif number == 3:
-        name_to_number = 'lizard'
-    elif number == 4:
-        name_to_number = 'scissors'
-    else :
+        name = "lizard"
+    elif number == 4: 
+        name = "scissors"    
+    else:
         print "Name not in this game!"
-        
-    return name_to_number
-
-    # convert number to a name using if/elif/else
-    # don't forget to return the result!
+    
+    return name     # return the result!
 
     
 def name_to_number(name):
     # fill in your code below
-    if name == "rock":
-        number_to_name = 0
-    elif name == "Spock":
-        number_to_name = 1
-    elif name == "paper":
-        number_to_name = 2
-    elif name == "lizard":
-        number_to_name = 3
-    elif name == "scissors":
-        number_to_name = 4
-    else :
-        print "invalid Number"
     
-    return number_to_name
-    # convert name to number using if/elif/else
-    # don't forget to return the result!
+    if name == "rock": # convert name to number using if/elif/else
+        number = 0
+    elif name == "Spock":
+        number = 1
+    elif name == "paper":
+        number = 2
+    elif name == "lizard":
+        number = 3
+    elif name == "scissors":
+        number = 4
+    else:
+        print "Invalid number" 
+    
+    return number    # return the result!
 
 
 #def rpsls(name): 
     # fill in your code below
-    
-name = name_to_number # name = name_to_number
-name_to_number = player_number # convert name to player_number using name_to_number
-comp_number = random.range(5)# compute random guess for comp_number using random.randrange()
-  
-difference = (player_number - comp_number)% 5# compute difference of player_number and comp_number modulo five
-# use if/elif/else to determine winner
-    if difference == 1 or 2:
-        winner = Player 
+
+    name = name_to_number
+    name_to_number == player_number    # convert name to player_number using name_to_number
+
+    comp_number = random.range(5)    # compute random guess for comp_number using random.randrange()
+
+    difference = (player_number - comp_number)% 5 # compute difference of player_number and comp_number modulo five
+
+    if difference == 1 or 2:    # use if/elif/else to determine winner
+        winner = "Player wins!"
     elif difference == 3 or 4:
-        winner = Computer 
+        winner = "Computer wins!"
+    elif difference == 0:
+        winner = "Player and Computer tie"
     else:
-        winner = Player and Computer tie
+        "Invalid turn"
+        
     
- comp_number == number_to_name # convert comp_number to name using number_to_name
+    comp_number == number_to_name    # convert comp_number to name using number_to_name
     
-# print results
-print "Player chooses" player_number
+print "Player chooses" player_number    # print results
 print "Computer chooses" comp_number
-print 
+print winner
+
     
 # test your code
 #rpsls("rock")
@@ -90,16 +91,18 @@ print
 #rpsls("scissors")
 
 # always remember to check your completed program against the grading rubric
+###################################################
+# Test calls to name_to_number()
+#print number_to_name(0)
+#print number_to_name(1)
+#print number_to_name(2)
+#print number_to_name(3)
+#print number_to_name(4)
 
-print number_to_name(0)
-print number_to_name(1)
-print number_to_name(2)
-print number_to_name(3)
-print number_to_name(4)
-print name_to_number("rock")
-print name_to_number("Spock")
-print name_to_number("paper")
-print name_to_number("lizard")
-print name_to_number("scissors")
-
-
+###################################################
+# Test calls to name_to_number()
+#print name_to_number("rock")
+#print name_to_number("Spock")
+#print name_to_number("paper")
+#print name_to_number("lizard")
+#print name_to_number("scissors")
