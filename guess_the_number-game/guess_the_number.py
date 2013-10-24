b173c2b05c2d3d0e6ca70c40a80f8f5c5f6cb816
Tania Chin <cybercorpindustries@gmail.com>
2013-10-24 18:33:38 -0400

    1.1.3
    Template for guess the number game.

---
+++ guess_the_number-game/guess_the_number.py
@@ -0,0 +1,54 @@
+'''
+Created on Oct 24, 2013
+
+@author: Tania
+'''
+# template for "Guess the number" mini-project
+# input will come from buttons and an input field
+# all output for the game will be printed in the console
+
+
+
+# initialize global variables used in your code
+
+
+
+# helper function to start and restart the game
+def new_game():
+    # remove this when you add your code    
+    pass
+
+
+# define event handlers for control panel
+def range100():
+    # button that changes range to range [0,100) and restarts
+    
+    # remove this when you add your code    
+    pass
+
+def range1000():
+    # button that changes range to range [0,1000) and restarts
+    
+    # remove this when you add your code    
+    pass
+    
+def input_guess(guess):
+    # main game logic goes here    
+    
+    # remove this when you add your code
+    pass
+
+    
+# create frame
+
+
+
+# register event handlers for control elements
+
+
+
+# call new_game and start frame
+
+
+
+# always remember to check your completed program against the grading rubric
