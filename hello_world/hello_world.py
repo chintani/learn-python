7411858477c754218f3d26ec1c610026c137e1b7
Tania Chin <cybercorpindustries@gmail.com>
2013-10-27 15:48:48 -0400

    1.1.5
    Hello World!

---
+++ hello_world/hello_world.py
@@ -0,0 +1,14 @@
+# Type: Interpreter
+# Prints "Hello, World!"
+# These lines of text are comments. They are not part of the program
+# they serve to remind us about its properties. The first line tell
+# us what to type to compile and test the program. The next line describes
+# the purpose of the program. 
+'''
+Created on Oct 26, 2013
+
+@author: Tania
+'''
+
+
+print "Hello, World!"
