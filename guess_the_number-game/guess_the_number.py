3a92d9c1e366b625b4bd1156da521851eaa6458b
Tania Chin <cybercorpindustries@gmail.com>
2013-11-01 22:38:36 -0400

    1.1.7
    Commented out code to test if the global variable section code works.

--- guess_the_number-game/guess_the_number.py
+++ guess_the_number-game/guess_the_number.py
@@ -17,45 +17,45 @@ num_range = 100
 print random.randint(1,100)
 
 # helper function to start and restart the game
-def new_game():
+#def new_game():
     # remove this when you add your code    
-    pass
+    #pass
 
 
 # define event handlers for control panel
-def range100():
+#def range100():
     # button that changes range to range [0,100) and restarts
     
     # remove this when you add your code    
-    pass
+    #pass
 
-def range1000():
+#def range1000():
     # button that changes range to range [0,1000) and restarts
     
     # remove this when you add your code    
-    pass
+    #pass
     
-def input_guess(guess):
+#def input_guess(guess):
     # main game logic goes here    
     
     # remove this when you add your code
-    pass
+    #pass
 
     
 # create frame
-frame = simplegui.create_frame('Guess The Number' 200, 200)
+#frame = simplegui.create_frame('Guess The Number' 200, 200)
 
 
 # register event handlers for control elements
 
-frame.add_button("Range is [0, 100]", range100, 200)
-frame.add_button("Range is [0, 1000]", range1000, 200)
-frame.add_input("Guess a number", get_input, 200)
+#frame.add_button("Range is [0, 100]", range100, 200)
+#frame.add_button("Range is [0, 1000]", range1000, 200)
+#frame.add_input("Guess a number", get_input, 200)
 
 # call new_game and start frame
-new_game()
+#new_game()
 
 
 # always remember to check your completed program against the grading rubric
 
-#URL to keep track of my work in codeskulptor:http://www.codeskulptor.org/#user21_ReT3kya23l_0.py
+#URL to keep track of my work in codeskulptor:http://www.codeskulptor.org/#user22_ReT3kya23l_1.py
