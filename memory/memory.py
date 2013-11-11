c3281949e426e6aaee47e56111280bf1c682a273
Tania Chin <cybercorpindustries@gmail.com>
2013-11-10 20:28:17 -0500

    1.2.4
    Memory game.

---
+++ memory/memory.py
@@ -0,0 +1,41 @@
+'''
+Created on Nov 10, 2013
+
+@author: Tania
+'''
+# implementation of card game - Memory
+
+import simplegui
+import random
+
+# helper function to initialize globals
+def new_game():
+    pass  
+
+     
+# define event handlers
+def mouseclick(pos):
+    # add game state logic here
+    pass
+    
+                        
+# cards are logically 50x100 pixels in size    
+def draw(canvas):
+    pass
+
+
+# create frame and add a button and labels
+frame = simplegui.create_frame("Memory", 800, 100)
+frame.add_button("Restart", new_game)
+label = frame.add_label("Turns = 0")
+
+# register event handlers
+frame.set_mouseclick_handler(mouseclick)
+frame.set_draw_handler(draw)
+
+# get things rolling
+new_game()
+frame.start()
+
+
+# Always remember to review the grading rubric
