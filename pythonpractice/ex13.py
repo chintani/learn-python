b10953322d8189fe82113d9d5a3b8c1cb893552f
Tania Chin <cybercorpindustries@gmail.com>
2013-12-01 20:45:31 -0500

    1.3.8

---
+++ pythonpractice/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second varible is:", second
+print "Your third varible is:", third
