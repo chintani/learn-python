5bbf758341e8bea7877d312b45dfc0efcee38b7a
Tania Chin <cybercorpindustries@gmail.com>
2014-01-15 20:22:33 -0500

    1.8.9

--- pythonpractice/correcting_coders_code_ex26.py
+++ pythonpractice/correcting_coders_code_ex26.py
@@ -66,7 +66,7 @@ print "This should be five: %s" % five
 
 def secret_formula(started):
     jelly_beans = started * 500
-    jars = jelly_beans \ 1000
+    jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
 
