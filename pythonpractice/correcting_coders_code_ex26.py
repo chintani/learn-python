bc864d5bdc59a0a4835e4d3e6bc0f0d5e5e79f35
Tania Chin <cybercorpindustries@gmail.com>
2014-01-16 22:31:21 -0500

    1.9.2
    To correct error: Traceback (most recent call last):
    File "(stdin)", line 1, in (module)
    File "correcting_coders_code_ex26.py", line 93
    .print_first_word(sorted_words)
    SyntaxError: invalid syntax

--- pythonpractice/correcting_coders_code_ex26.py
+++ pythonpractice/correcting_coders_code_ex26.py
@@ -90,7 +90,7 @@ sorted_words = ex25.sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
-.print_first_word(sorted_words)
+print_first_word(sorted_words)
 print_last_word(sorted_words)
 sorted_words = ex25.sort_sentence(sentence)
 prin sorted_words
