4508e3a690aed868d8efe8f65ef881ac9a30286a
Tania Chin <cybercorpindustries@gmail.com>
2014-01-18 00:07:13 -0500

    1.9.5
    To correct error:  Traceback (most recent call last):
    File "<stdin>", line 1, in <module>
    File "correcting_coders_code_ex26.py", line 75, in <module>
    beans, jars, crates == secret_formula(start-point)
    NameError: name 'beans' is not defined

--- pythonpractice/correcting_coders_code_ex26.py
+++ pythonpractice/correcting_coders_code_ex26.py
@@ -72,7 +72,7 @@ def secret_formula(started):
 
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)
+jelly_beans, jars, crates == secret_formula(start-point)
 
 print "With a starting point of: %d" % start_point
 print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
