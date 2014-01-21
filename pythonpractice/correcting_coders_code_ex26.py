4420a05fe88ddb5443969c70437d0de0eb41678d
Tania Chin <cybercorpindustries@gmail.com>
2014-01-21 18:13:49 -0500

    1.2.0
    To fix typographical error "crates" instead of "crabapples."

--- pythonpractice/correcting_coders_code_ex26.py
+++ pythonpractice/correcting_coders_code_ex26.py
@@ -80,7 +80,7 @@ print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
 
 
 sentence = "All good things come to those who wait."
